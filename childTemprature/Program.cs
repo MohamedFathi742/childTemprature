a360aa6dfc09c19e3d1a8fbeaa68e0612813d89a
MohamedFathi742 <mofathi742002@gmail.com>
2024-10-18 11:28:20 +0300

    Add project files.

---
+++ childTemprature/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace childTemprature
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(" welcome to child temprature app ");
+            //ask user to enter temprature
+            Console.WriteLine("please enter temprature");
+            //read temprature
+            double temprature = 0;
+            bool isCnverted = double.TryParse(Console.ReadLine(), out temprature);
+            //if
+            if (temprature == 37 || temprature == 37.5)
+            {
+                Console.WriteLine("The child's temperature is good.");
+            }
+
+            else
+            {
+                Console.WriteLine("The child's temperature is bad.");
+
+            }
+        }
+    }
+}
